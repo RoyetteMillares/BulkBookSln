cb031549c1519ad0f9778a17cb2e73ca695ae7c9
SHORE360 i3 FRMTD <SHORE360 i3 FRMTD@ROYETTE>
2022-12-29 17:46:21 +0800

    Add project files.

---
+++ BulkBookWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkBookWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
